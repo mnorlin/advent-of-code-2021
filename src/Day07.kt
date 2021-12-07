a685ec73641875c7a05a3114975482767986b788
Martin Norlin <1668384+mnorlin@users.noreply.github.com>
2021-12-07 21:54:42 +0100

    Add day 6 and 7

---
+++ src/Day07.kt
@@ -0,0 +1,19 @@
+import kotlin.math.abs
+
+fun main() {
+
+    fun part1(crabs: List<Int>): Int {
+        val median = crabs.sorted()[crabs.count() / 2]
+        return crabs.sumOf { abs(it - median) }
+    }
+
+    fun part2(crabs: List<Int>): Int {
+        val max = crabs.maxOrNull()
+        return (0..(max ?: 0)).map { position -> crabs.sumOf { abs(it - position).triangular() } }.minOrNull() ?: 0
+    }
+
+    val input = readInput("Day07")
+    val crabs = input[0].split(",").map { it.toInt() }
+    println(part1(crabs))
+    println(part2(crabs))
+}
